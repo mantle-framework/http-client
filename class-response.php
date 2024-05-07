c2b1738e694ef3a5f88fea05e736d1515c2344da
mantle-ci <mantle@alley.com>
2024-05-07 18:21:41 +0000

    Ensure Composer is properly loaded during unit tests when installing (#542)

--- class-response.php
+++ class-response.php
@@ -26,21 +26,34 @@ class Response implements ArrayAccess {
 	use Macroable;
 
 	/**
-	 * The decoded JSON response.
+	 * Raw response from `wp_remote_request()`.
+	 *
+	 * @var array
 	 */
-	protected ?array $decoded = null;
+	protected array $response;
+
+	/**
+	 * The decoded JSON response.
+	 *
+	 * @var array|null
+	 */
+	protected ?array $decoded;
 
 	/**
 	 * The decoded XML Element response.
+	 *
+	 * @var SimpleXMLElement|null
 	 */
-	protected ?SimpleXMLElement $element = null;
+	protected ?SimpleXMLElement $element;
 
 	/**
 	 * Constructor.
 	 *
 	 * @param array $response Raw response from `wp_remote_request()`.
 	 */
-	public function __construct( protected array $response ) {
+	public function __construct( array $response ) {
+		$this->response = $response;
+
 		// Format the headers to be lower-case.
 		$this->response['headers'] = array_change_key_case( (array) ( $this->response['headers'] ?? [] ) );
 	}
@@ -83,6 +96,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Retrieve the raw response from `wp_remote_request()`.
+	 *
+	 * @return array
 	 */
 	public function response(): array {
 		return $this->response;
@@ -90,6 +105,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Retrieve all the headers from a response.
+	 *
+	 * @return array
 	 */
 	public function headers(): array {
 		return (array) ( $this->response['headers'] ?? [] );
@@ -108,6 +125,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Retrieve the status code for the response.
+	 *
+	 * @return int
 	 */
 	public function status(): int {
 		return (int) ( $this->response['response']['code'] ?? 0 );
@@ -115,34 +134,44 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Determine if the request was successful.
+	 *
+	 * @return bool
 	 */
-	public function successful(): bool {
+	public function successful() {
 		return $this->status() >= 200 && $this->status() < 300;
 	}
 
 	/**
 	 * Determine if the response code was "OK".
+	 *
+	 * @return bool
 	 */
-	public function ok(): bool {
+	public function ok() {
 		return $this->status() === 200;
 	}
 
 	/**
 	 * Determine if the response code was not found (404).
+	 *
+	 * @return bool
 	 */
-	public function not_found(): bool {
+	public function not_found() {
 		return $this->status() === 404;
 	}
 
 	/**
 	 * Determine if the response was a redirect.
+	 *
+	 * @return bool
 	 */
-	public function redirect(): bool {
+	public function redirect() {
 		return $this->status() >= 300 && $this->status() < 400;
 	}
 
 	/**
 	 * Determine if the response was a 401 "Unauthorized" response.
+	 *
+	 * @return bool
 	 */
 	public function unauthorized(): bool {
 		return $this->status() === 401;
@@ -150,6 +179,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Determine if the response was a 403 "Forbidden" response.
+	 *
+	 * @return bool
 	 */
 	public function forbidden(): bool {
 		return $this->status() === 403;
@@ -157,6 +188,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Determine if the response indicates a client or server error occurred.
+	 *
+	 * @return bool
 	 */
 	public function failed(): bool {
 		return $this->server_error() || $this->client_error() || $this->is_wp_error();
@@ -164,6 +197,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Determine if the response indicates a client error occurred.
+	 *
+	 * @return bool
 	 */
 	public function client_error(): bool {
 		return $this->status() >= 400 && $this->status() < 500;
@@ -171,6 +206,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Determine if the response indicates a server error occurred.
+	 *
+	 * @return bool
 	 */
 	public function server_error(): bool {
 		return $this->status() >= 500;
@@ -178,6 +215,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Check if the error was an WP_Error.
+	 *
+	 * @return bool
 	 */
 	public function is_wp_error(): bool {
 		return ! empty( $this->response['is_wp_error'] );
@@ -185,9 +224,11 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Check if the response is JSON.
+	 *
+	 * @return bool
 	 */
 	public function is_json(): bool {
-		if ( false !== strpos( (string) $this->header( 'content-type' ), 'application/json' ) ) {
+		if ( false !== strpos( $this->header( 'content-type' ), 'application/json' ) ) {
 			return true;
 		}
 
@@ -197,13 +238,15 @@ class Response implements ArrayAccess {
 	/**
 	 * Check if the response is XML. Does not validate if the response is a valid
 	 * XML document.
+	 *
+	 * @return bool
 	 */
 	public function is_xml(): bool {
-		if ( false !== strpos( (string) $this->header( 'content-type' ), 'application/xml' ) ) {
+		if ( false !== strpos( $this->header( 'content-type' ), 'application/xml' ) ) {
 			return true;
 		}
 
-		return str_starts_with( trim( strtolower( $this->body() ) ), '<?xml' );
+		return 0 === strpos( trim( strtolower( $this->body() ) ), '<?xml' );
 	}
 
 	/**
@@ -217,6 +260,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Retrieve the file path to the downloaded file.
+	 *
+	 * @return string|null
 	 */
 	public function file(): ?string {
 		return $this->response['filename'] ?? null;
@@ -224,6 +269,8 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Retrieve the file contents of the downloaded file.
+	 *
+	 * @return string|null
 	 */
 	public function file_contents(): ?string {
 		return ! empty( $this->response['filename'] ) ? file_get_contents( $this->file() ) : null; // phpcs:ignore WordPressVIPMinimum.Performance.FetchingRemoteData.FileGetContentsUnknown
@@ -304,6 +351,7 @@ class Response implements ArrayAccess {
 	 * Retrieve a specific cookie by name.
 	 *
 	 * @param string $name Cookie name.
+	 * @return WP_Http_Cookie|null
 	 */
 	public function cookie( string $name ): ?WP_Http_Cookie {
 		return collect( $this->cookies() )
@@ -323,8 +371,10 @@ class Response implements ArrayAccess {
 
 	/**
 	 * Dump the response to the screen and exit.
+	 *
+	 * @return void
 	 */
-	public function dd(): void {
+	public function dd() {
 		$this->dump();
 		exit( 1 );
 	}
@@ -333,6 +383,7 @@ class Response implements ArrayAccess {
 	 * Check if an attribute exists on the response.
 	 *
 	 * @param mixed $offset Offset to check.
+	 * @return bool
 	 */
 	public function offsetExists( mixed $offset ): bool {
 		if ( $this->is_xml() ) {
@@ -346,6 +397,7 @@ class Response implements ArrayAccess {
 	 * Retrieve an attribute from the response.
 	 *
 	 * @param mixed $offset Offset to get.
+	 * @return mixed
 	 */
 	public function offsetGet( mixed $offset ): mixed {
 		if ( $this->is_xml() ) {
@@ -362,6 +414,7 @@ class Response implements ArrayAccess {
 	 *
 	 * @param mixed $offset Offset.
 	 * @param mixed $value Value.
+	 * @return void
 	 */
 	public function offsetSet( mixed $offset, mixed $value ): void {
 		throw new LogicException( 'Response values are read-only.' );
@@ -372,6 +425,7 @@ class Response implements ArrayAccess {
 	 *
 	 * @throws LogicException Not supported on responses.
 	 * @param mixed $offset Offset.
+	 * @return void
 	 */
 	public function offsetUnset( mixed $offset ): void {
 		throw new LogicException( 'Response values are read-only.' );
