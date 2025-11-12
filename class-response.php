ece1b47d5ca3c3aa6da09b5e25069ffd0ba518f9
mantle-ci <mantle@alley.com>
2025-11-12 14:44:16 +0000

    Release v1.13.4

--- class-response.php
+++ class-response.php
@@ -5,8 +5,6 @@
  * @package Mantle
  */
 
-declare(strict_types=1);
-
 namespace Mantle\Http_Client;
 
 use ArrayAccess;
@@ -309,9 +307,7 @@ class Response implements ArrayAccess {
 	 * Retrieve the file contents of the downloaded file.
 	 */
 	public function file_contents(): ?string {
-		$file = $this->file();
-
-		return empty( $file ) ? null : (string) file_get_contents( $file ); // phpcs:ignore WordPressVIPMinimum.Performance.FetchingRemoteData.FileGetContentsUnknown
+		return empty( $this->response['filename'] ) ? null : file_get_contents( $this->file() ); // phpcs:ignore WordPressVIPMinimum.Performance.FetchingRemoteData.FileGetContentsUnknown
 	}
 
 	/**
@@ -362,16 +358,11 @@ class Response implements ArrayAccess {
 	/**
 	 * Get the XML body of the response.
 	 *
-	 * @template TDefault
-	 *
-	 * @param string|null $xpath Path to pass to `SimpleXMLElement::xpath()`, optional.
-	 * @param mixed       $default Default value to return if the path does not exist.
+	 * @param string $xpath Path to pass to `SimpleXMLElement::xpath()`, optional.
+	 * @param string $default Default value to return if the path does not exist.
 	 * @return SimpleXMLElement|string|null Returns a specific SimpleXMLElement if path is specified, otherwise the entire document.
-	 *
-	 * @phpstan-param TDefault $default
-	 * @phpstan-return ($xpath is null ? SimpleXMLElement : (array<SimpleXMLElement>|TDefault))
 	 */
-	public function xml( ?string $xpath = null, mixed $default = null ) {
+	public function xml( ?string $xpath = null, $default = null ) {
 		if ( ! $this->element instanceof \SimpleXMLElement ) {
 			$previous = libxml_use_internal_errors( true );
 
